aa7a0bcf11091ab28c55b26c6aed4808e4a27db5
Magda098 <magjac22@gmail.com>
2023-01-30 09:48:59 +0300

    connecting to the database

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv')=constant();
 
-// Create an express app,Initialize express
-const app = express();
-
 // Define the port number
 const port = process.env.PORT || 3000;
 const databaseUrl =process.env.DATABASE_URL ||'';
+
+// Create an express app,Initialize express
+const app = express();
+
+//Connecting to the database
+const database=mongoose.connection;//initialize a mongoose connection
+//create a listener to the db
+database.on('error',(error)=>{
+    console.error(error);
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on PORT ${port}`);
